97361a1b9a3b66a1b032c629d9a58a0ca35a93be
Wolfstreak25 <108297565+Wolfstreak25@users.noreply.github.com>
2022-12-11 17:46:02 +0530

    Character movement
    adding movement for character using raycast to detect collisions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector2 movementInput;
+    Rigidbody2D rb; 
+    public float moveSpeed = 1f;
+    public float collisionOffset = 0.02f;
+    public ContactFilter2D movementFilter;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        if(movementInput != Vector2.zero){
+            int count = rb.Cast(movementInput,movementFilter,castCollisions,moveSpeed* Time.fixedDeltaTime + collisionOffset);
+            if(count == 0){
+                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+            }
+        }
         
+    }
+    void OnMove(InputValue movementvalue)
+    {
+         movementInput = movementvalue.Get<Vector2>();
     }
 }
